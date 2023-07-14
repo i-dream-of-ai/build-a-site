eec71fbe0caca970a1d799e82fc3631b069e4b71
J Hanlon <kitchenbeats@gmail.com>
2023-07-13 18:30:59 -0700

    dated image webhook with generateHTML

--- src/app/api/stable-d-webhook/route.ts
+++ src/app/api/stable-d-webhook/route.ts
@@ -2,7 +2,8 @@ import clientPromise from '@/lib/mongodb'
 
 import { NextRequest, NextResponse } from 'next/server'
 
-import { uploadImagesToS3 } from '@/utils/s3'
+import { uploadHTMLToS3, uploadImagesToS3 } from '@/utils/s3'
+import { generateHTML } from '@/utils/generate/html'
 
 const dbName = process.env.MONGODB_DB
 
@@ -12,8 +13,9 @@ export async function POST(req: NextRequest) {
 
   try {
     const client = await clientPromise
-    const collection = client.db(dbName).collection('images')
-    const doc = await collection.findOneAndUpdate(
+    const imagesCollection = client.db(dbName).collection('images');;
+
+    const imageDoc = await imagesCollection.findOneAndUpdate(
       { bucketName: trackData.bucketName, name: trackData.name },
       { $set: imageData },
       {
@@ -21,22 +23,51 @@ export async function POST(req: NextRequest) {
         returnDocument: 'after',
       },
     )
-    if (doc.value && doc.value.status === 'success') {
-      const images: { [key: string]: string[] } = {}
+    if (imageDoc.value && imageDoc.value.status === 'success') {
+
+      const sitesCollection = client.db(dbName).collection('sites');;
+      const site = await sitesCollection.findOne({bucketName: trackData.bucketName})
+
+      if(!site) return NextResponse.json({ error:"Site not found." }, { status: 500 });
+
+      const siteData = site.content;
+
+      const timestamp = Date.now();
+
+      let hasSSL = false
+      //if we have a domain assume we have ssl
+      if(site.domain) {
+        hasSSL = true
+        siteData[trackData.name+'URL'] = `https://${site.domain}/${trackData.name}-0.png?${timestamp}`
+      } else {
+        siteData[trackData.name+'URL'] = `http://${site.bucketName}.s3-website.${process.env.AWS_REGION}.amazonaws.com/${trackData.name}-0.png?${timestamp}`
+      }
+
+      const res = await sitesCollection.updateOne({bucketName: trackData.bucketName},{
+        $set: siteData
+      })
+
+      const images: { [key: string]: string[] } = {};
+
       images[trackData.name].push(imageData.output[0])
 
       await uploadImagesToS3(images, trackData.bucketName)
 
-      //collection.deleteOne()
+      //create html, and mark isSSL true since we have a domain
+      const { html } = await generateHTML(siteData, site.domain || site.bucketName, hasSSL)
 
-      console.log('webhook doc: ', doc)
+      //update file in bucket
+      await uploadHTMLToS3(html, site.domain || site.bucketName)
+
+      imagesCollection.deleteOne({_id: imageDoc.value._id})
+
       return NextResponse.json(
-        { message: 'Image created successfully' },
+        { message: 'Image updated successfully' },
         { status: 201 },
       )
     }
 
-    console.error('Image webhook error: ', doc)
+    console.error('Image webhook error: ', imageDoc)
     return NextResponse.json({ message: 'Image not updated.' }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 })
