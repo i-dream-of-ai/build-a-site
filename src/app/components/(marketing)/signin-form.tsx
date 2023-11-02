76004172e9bd11ddaa43458253714e0ae9ab25b0
Inside4ndroid <65467186+Inside4ndroid@users.noreply.github.com>
2023-11-02 15:01:06 +0000

    Update signin-form.tsx
    added extra thing for forgot password

--- src/app/components/(marketing)/signin-form.tsx
+++ src/app/components/(marketing)/signin-form.tsx
@@ -15,6 +15,39 @@ export default function SigninForm() {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  function toggleModal() {
+    setShowModal(!showModal);
+    if(showModal){
+      const email = document.getElementById('email').value;
+      saveToken(email).then(r => toast.success('A password verify link has been sent to your email.'));
+    }
+  }
+
+  async function saveToken(email: string) {
+    try {
+      const response = await fetch('/api/setToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to save token');
+      }
+
+      const data = await response.json();
+      if(data){
+
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
   async function submit(e: any) {
     e.preventDefault()
     isWorking(true)
@@ -26,7 +59,6 @@ export default function SigninForm() {
       toast.error('You must include your password!')
       return
     }
-    console.log('email', email)
 
     const status = await signIn('credentials', {
       email: email,
@@ -110,39 +142,92 @@ export default function SigninForm() {
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              className="h-4 w-4 rounded border-purple-300 text-purple-600 focus:ring-purple-600"
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-purple-300 text-purple-600 focus:ring-purple-600"
             />
             <label
-              htmlFor="remember-me"
-              className="ml-3 block text-sm leading-6 text-purple-400"
+                htmlFor="remember-me"
+                className="ml-3 block text-sm leading-6 text-purple-400"
             >
               Remember me
             </label>
           </div>
 
-          <div className="text-sm leading-6">
-            <a
-              href="#"
-              className="font-semibold text-purple-400 hover:text-purple-500"
-            >
-              Forgot password?
-            </a>
+          <div
+              className="ml-3 block text-sm leading-6 text-purple-400"
+              onClick={toggleModal}
+          >
+            Forgot password?
           </div>
         </div>
 
         <div>
           <button
-            type="submit"
-            disabled={working}
-            className={`flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 disabled:animate-pulse`}
+              type="submit"
+              disabled={working}
+              className={`flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 disabled:animate-pulse`}
           >
             Sign in
           </button>
         </div>
       </form>
+      {showModal && (
+          <div className="bg-black fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-greydark p-6 rounded shadow-lg w-96">
+              <div className="mb-8 mx-auto text-center">
+                <Image
+                    className={`${working && 'animate-pulse'} mx-auto max-w-[150px]`}
+                    src="/ufo.svg"
+                    width={200}
+                    height={200}
+                    alt="logo"
+                />
+                <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-200">
+                  Password Reset
+                </h2>
+              </div>
+
+              <form onSubmit={submit} method="POST" className="space-y-6 text-center">
+                <div>
+                  <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-purple-500"
+                  >
+                    Your Registered Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="text-center block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-purple-300 placeholder:text-purple-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6 bg-gray-800"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                      type="submit"
+                      disabled={working}
+                      onClick={toggleModal}
+                      className={`flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 disabled:animate-pulse`}
+                  >
+                    Reset Password
+                  </button>
+                </div>
+              </form>
+              <br/>
+              <br/>
+              <button onClick={toggleModal} className="flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 disabled:animate-pulse">Cancel</button>
+            </div>
+          </div>
+      )}
     </div>
   )
 }
